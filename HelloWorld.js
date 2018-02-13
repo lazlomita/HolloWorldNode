6eaae9772dfc48e832a067a565bab2caef905fd0
lazlo.mita <lazlo.mita@gmail.com>
2018-02-13 17:52:11 -0400

    Add FOR, IF, ELSE, Concatenation.

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,3 +1,21 @@
-// Print Hello world in Node programming language
+/******************************************************************************
+ *  Compilation:  No copile process needed
+ *  Execution:    node HelloWorld.js
+ *
+ * Print Hello world in JavaScript programming language for node
+ *
+ ******************************************************************************/// Print Hello world in Node programming language
 // Call the console.log function.
 console.log("Hello world NODE\n");
+
+// Run a cycle that goes from 1 to 10
+for (i = 1; i <= 10; i++) {
+	// Decide if the number is even or not
+	if( i%2 == 0){
+		// Text concatenation and print
+		console.log("Count is: " + i + " and it is even.");
+	} else {
+		// Text concatenation and print
+		console.log("Count is: " + i + " and it is odd.");
+	}
+}
